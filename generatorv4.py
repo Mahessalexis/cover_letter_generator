f4b5ab28904e08cd38972950a32d14df87356634
Mahessalexis <maes.alexis3131@gmail.com>
2026-01-05 21:48:36 +0100

    Fix download URL for subdirectory deployment

--- generatorv4.py
+++ generatorv4.py
@@ -1074,7 +1074,7 @@ HTML_TEMPLATE = """
                                     </svg>
                                     Copier
                                 </button>
-                                <a href="/download/docx" class="btn btn-download">
+                                <a href="download/docx" class="btn btn-download">
                                     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4M7 10l5 5 5-5M12 15V3"/>
                                     </svg>
